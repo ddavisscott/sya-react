3205fc58b4a1b413bc866c61a3d1c7b2d6b93aa4
cortiz319 <christianortiz319@yahoo.com>
2018-11-01 17:04:15 -0700

    Merged Sophies and Brandons branch

--- SYA-React-App/src/App.js
+++ SYA-React-App/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from './AppBar.js';
+import Demo from './demo.js';
+
 import {Provider } from 'react-redux';
 
 import Posts from './components/Posts'; 
@@ -11,10 +15,17 @@ import store from './store';
 import SignIn from './components/SignIn';
 import AristSignUp from './components/ArtistSignUp';
 
+
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
+      <React.Fragment>
+      <CssBaseline />
+      <AppBar />
+      <Demo />
+      </React.Fragment>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
--- SYA-React-App/src/index.js
+++ SYA-React-App/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Demo from './demo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Demo />, document.querySelector('#root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
