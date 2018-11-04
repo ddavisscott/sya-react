f8b1a59d9e4bdb94bb53b6e6d6a5ac13bd4313de
Kavan Samra <kssamra@ucsc.edu>
2018-11-03 19:04:16 -0700

    index.js and the entire project should now be configured to use Amplify

--- SYA-React-App/src/index.js
+++ SYA-React-App/src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Demo from './demo';
-import Hello from 'hell';
 ReactDOM.render(<Demo />, document.querySelector('#root'));
 
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+
+Amplify.configure(config)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
