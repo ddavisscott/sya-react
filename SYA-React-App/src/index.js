d8f9988785ec15c926a91782b2a41eacd3cfbd0d
Kavan Samra <kssamra@ucsc.edu>
2018-11-03 18:56:07 -0700

    Started rebuilding aws cognito signin page in the main repo

--- SYA-React-App/src/index.js
+++ SYA-React-App/src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Demo from './demo';
-
+import Hello from 'hell';
 ReactDOM.render(<Demo />, document.querySelector('#root'));
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
